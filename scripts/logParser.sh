abc84c23494f1ef524029327af8456d7330acdc2
Avaneesh Desai <avaneesh.desai@avaneeshd-ltm5.internal.salesforce.com>
2019-12-19 12:33:48 -0800

    Adding infinite loop to parse log every 10 seconds

--- scripts/logParser.sh
+++ scripts/logParser.sh
@@ -1,4 +1,4 @@
-SOURCE_LOG_URL=./sample_output.log
+SOURCE_LOG_URL=$1
 JSON_REGEX='(.*)(LightningInteraction`v0\.2`)(\{.*\:\{.*\:.*\}\}.*})'
 MONGO_DB_CONNECTION='127.0.0.1/beacon-db'
 
@@ -8,7 +8,7 @@ echo "Scanning output.log for search-beacon logs..."
 boundaries=$(mongo --quiet $MONGO_DB_CONNECTION --eval 'printjson(db.featureboundaries.find({ }, { _id : 0 }).toArray())')
 
 # Finds the region for a given point.
-# x - {Number} X coordinate 
+# x - {Number} X coordinate
 # y - {Number} Y coordinate
 find_region()
 {
@@ -16,10 +16,10 @@ find_region()
   y=$2;
   rows=$( echo $boundaries | jq '. | length') # Get all region boundaries
   while [ $rows -gt 0 ]; do
-	
-	  x1=$(echo $boundaries | jq ".[$rows-1] .topLeftX");
+    
+      x1=$(echo $boundaries | jq ".[$rows-1] .topLeftX");
     x2=$(echo $boundaries | jq ".[$rows-1] .bottomRightX");
-	  y1=$(echo $boundaries | jq ".[$rows-1] .topLeftY");
+      y1=$(echo $boundaries | jq ".[$rows-1] .topLeftY");
     y2=$(echo $boundaries | jq ".[$rows-1] .bottomLeftY");
     rgn_name=$(echo $boundaries | jq ".[$rows-1] .featureName");
         
@@ -27,29 +27,36 @@ find_region()
     if [ $x -ge $x1 ] && [ $x -le $x2 ] && [ $y -ge $y1 ] && [ $y -le $y2 ]; then
       echo $rgn_name;
     fi
-	  rows=$((rows-1));	
+      rows=$((rows-1));
   
   done
 }
 
 ############## MAIN #######################
 # read logs and get search-beacon logs
-result=$(cat $SOURCE_LOG_URL | grep '.*Search-Beacon.*');
-echo "Parsing the logs..."
+if [ -z "$1" ]; then
+ echo "No log file supplied... exiting."
 
-echo "$result" | \
-while IFS= read -r line; do
-    region=''
-    if [[ $line =~ $JSON_REGEX ]]
-    then
-        json="${BASH_REMATCH[3]}" # Get JSON data
-        data=$(echo "$json" | jq '.locator.context');
-        x=$(echo "$data" | jq '.pageX');
-        y=$(echo "$data" | jq '.pageY');
-        region=$(find_region $x $y); # Get region for the coordinate
-        
-        mongo --quiet $MONGO_DB_CONNECTION --eval "var region = '$region'; var x='$x'; var y='$y';" dbscript.js
-    else
-        echo "$line doesn't match" >&2 # this could get noisy if there are a lot of non-matching logs
-    fi
-done
+else
+  while true; do
+    result=$(cat $SOURCE_LOG_URL | grep '.*Search-Beacon.*');
+    echo "Parsing the logs..."
+
+    echo "$result" | \
+    while IFS= read -r line; do
+        region=''
+        if [[ $line =~ $JSON_REGEX ]]; then
+            json="${BASH_REMATCH[3]}" # Get JSON data
+            data=$(echo "$json" | jq '.locator.context');
+            x=$(echo "$data" | jq '.pageX');
+            y=$(echo "$data" | jq '.pageY');
+            region=$(find_region $x $y); # Get region for the coordinate
+            
+            mongo --quiet $MONGO_DB_CONNECTION --eval "var region = '$region'; var x='$x'; var y='$y';" dbscript.js
+        else
+            echo "$line doesn't match" >&2 # this could get noisy if there are a lot of non-matching logs
+        fi
+    done
+    sleep 10;
+  done
+fi

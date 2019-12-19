f3ef14161ce9f80cb1ac6c0d905ce0ae17842c04
Avaneesh Desai <avaneesh.desai@avaneeshd-ltm5.internal.salesforce.com>
2019-12-19 15:11:23 -0800

    bug fixes

--- scripts/dbscript.js
+++ scripts/dbscript.js
@@ -5,7 +5,7 @@ switch(region){
  	var res = db.recordpreviewclicks.find({x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}).toArray()[0];
  	if(res){
  		res.value = res.value ? res.value:0;
- 		db.recordpreviewclicks.update({{x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}, {$set: {value: res.value + 1}});
+ 		db.recordpreviewclicks.update({x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}, {$set: {value: res.value + 1}});
  	}
  	else{
 		db.recordpreviewclicks.insert(document);

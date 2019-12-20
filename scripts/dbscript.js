3d7aa7d97201d1dc1c8d5b41458faa91d5b601d2
Avaneesh Desai <avaneesh.desai@avaneeshd-ltm5.internal.salesforce.com>
2019-12-19 19:43:56 -0800

    Normalization before finding regions+ support for  more regions

--- scripts/dbscript.js
+++ scripts/dbscript.js
@@ -1,5 +1,5 @@
 var document = { coordinates : '(' + x + ', ' + y + ')', x: x, y: y, windowWidth: windowWidth, windowHeight: windowHeight, value : 1 };
-print(windowWidth, windowHeight);
+
 switch(region){
  case '"recordPreview"':
  	var res = db.recordpreviewclicks.find({x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}).toArray()[0];
@@ -11,5 +11,25 @@ switch(region){
 		db.recordpreviewclicks.insert(document);
 	}
 	break;
+  case '"searchSuggestions"':
+ 	var res = db.searchsuggestionclicks.find({x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}).toArray()[0];
+ 	if(res){
+ 		res.value = res.value ? res.value:0;
+ 		db.searchsuggestionclicks.update({x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}, {$set: {value: res.value + 1}});
+ 	}
+ 	else{
+		db.searchsuggestionclicks.insert(document);
+	}
+	break;
+	case '"leftNav"':
+ 	var res = db.leftnavclicks.find({x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}).toArray()[0];
+ 	if(res){
+ 		res.value = res.value ? res.value:0;
+ 		db.leftnavclicks.update({x: x, y: y,  windowWidth: windowWidth, windowHeight: windowHeight}, {$set: {value: res.value + 1}});
+ 	}
+ 	else{
+		db.leftnavclicks.insert(document);
+	}
+	break;	
  default:
 }
--- scripts/logParser.sh
+++ scripts/logParser.sh
@@ -1,6 +1,8 @@
 SOURCE_LOG_URL=$1
 JSON_REGEX='(.*)(LightningInteraction`v0\.2`)(\{.*\:\{.*\:.*\}\}.*})'
 MONGO_DB_CONNECTION='127.0.0.1/beacon-db'
+DEFAULT_WINDOW_X=1440
+DEFAULT_WINDOW_Y=798
 
 echo "Scanning output.log for search-beacon logs..."
 
@@ -8,12 +10,13 @@ echo "Scanning output.log for search-beacon logs..."
 boundaries=$(mongo --quiet $MONGO_DB_CONNECTION --eval 'printjson(db.featureboundaries.find({ }, { _id : 0 }).toArray())')
 
 # Finds the region for a given point.
-# x - {Number} X coordinate 
+# x - {Number} X coordinate
 # y - {Number} Y coordinate
 find_region()
 {
   x=$1;
   y=$2;
+  
   rows=$( echo $boundaries | jq '. | length') # Get all region boundaries
   while [ $rows -gt 0 ]; do
   
@@ -27,14 +30,14 @@ find_region()
     if [ $x -ge $x1 ] && [ $x -le $x2 ] && [ $y -ge $y1 ] && [ $y -le $y2 ]; then
       echo $rgn_name;
     fi
-    rows=$((rows-1)); 
+    rows=$((rows-1));
   
   done
 }
 
 ############## MAIN #######################
 # read logs and get search-beacon logs
-if [ -z "$1" ]; then 
+if [ -z "$1" ]; then
  echo "No log file supplied... exiting."
 
 else
@@ -52,6 +55,11 @@ else
             y=$(echo "$data" | jq '.pageY');
             windowWidth=$(echo "$data" | jq '.windowWidth');
             windowHeight=$(echo "$data" | jq '.windowHeight');
+
+            #Normalize w.r.t window size.
+            x=$((x*DEFAULT_WINDOW_X/windowWidth));
+            y=$((y*DEFAULT_WINDOW_Y/windowHeight));
+
             region=$(find_region $x $y); # Get region for the coordinate
             
             mongo --quiet $MONGO_DB_CONNECTION --eval "var region = '$region'; var x='$x'; var y='$y'; var windowWidth='$windowWidth'; var windowHeight='$windowHeight'" dbscript.js
